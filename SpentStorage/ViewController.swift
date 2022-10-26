1f76b5de00fd7c4adbaf2c6ca407722014bf14be
perevedniuk <perevedniuk@gmail.com>
2022-10-26 13:08:57 +0300

    Initial Commit

---
+++ SpentStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpentStorage
+//
+//  Created by andrey perevedniuk on 26.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
